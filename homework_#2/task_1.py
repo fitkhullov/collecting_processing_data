343dadfbbf19928348f6a7b446c0a18f942f12d0
fitkhullov <anton.fitkhullov@gmail.com>
2021-10-28 20:03:45 +0700

    add parser for 'salary' field

--- homework_#2/task_1.py
+++ homework_#2/task_1.py
@@ -2,8 +2,8 @@ import json
 
 import requests as rq
 from bs4 import BeautifulSoup as bs
-from pprint import pprint
 import json
+import re
 
 vac = input('Введите интересующую вакансию: ')
 url = 'https://novosibirsk.hh.ru'
@@ -13,7 +13,7 @@ params = {'cluster': 'true',
           'enable_snippets': 'true',
           'text': vac.replace(' ', '+'),
           'page': 0}
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'}
 
 vac_list = []
 while True:
@@ -43,6 +43,38 @@ while True:
     else:
         break
 
+# Парсим поле с зарплатой в словарь из полей min_salary, max_salary, valuta
+for i, vac in enumerate(vac_list):
+    salary_row = vac['salary']
+    min_val = None
+    max_val = None
+    valuta = None
+    if salary_row is None:
+        pass
+    else:
+        tmp_list = re.split('-| ', salary_row)
+        tmp_nums = []
+        tmp_idx = []
+        for j, x in enumerate(tmp_list):
+            try:
+                tmp_nums.append(float(x))
+            except:
+                tmp_list[j] = x.lower()
+                tmp_idx.append(j)
+        for k in tmp_idx:
+            if tmp_list[k].startswith('р'):
+                valuta = 'рублей'
+                break
+        if len(tmp_nums) == 2:
+            min_val = min(tmp_nums)
+            max_val = max(tmp_nums)
+        elif len(tmp_nums) == 1:
+            if 'от' in tmp_list:
+                min_val = tmp_nums[0]
+            else:
+                max_val = tmp_nums[0]
+
+    vac_list[i]['salary'] = {'min_salary': min_val, 'max_salary': max_val, 'valuta': valuta}
+
 with open('vacancies.json', 'w') as f:
     f.write(json.dumps(vac_list, ensure_ascii=False))
-
